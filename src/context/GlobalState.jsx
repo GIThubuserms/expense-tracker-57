895b4df1b8e52cd20e8e0ce8582b29ef11c05962
bsse23057 <murtazasaleem2004@gmail.com>
2024-08-05 15:48:12 +0500

    Empty the transactrions

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -2,10 +2,7 @@ import React, { useReducer, createContext, useContext } from "react";
 // Initial state
 const initialState = {
     transactions: [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: +300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: +150 }
+       
     ]
 };
 
